9e7412ef8b3627a64b0ffd28083ea49b0806552b
Davide Cristini <davcri91@gmail.com>
2020-03-19 09:16:58 +0100

    Add comments on how to change viewport size

--- Objects/Debug/ResolutionChanger.gd
+++ Objects/Debug/ResolutionChanger.gd
@@ -1,6 +1,8 @@
 extends Node
 
+
 export(bool) var change_aspect_ratio_only
+
 
 func _on_DebugOptions_resolution_selected(res) -> void:
 	var default_size = Vector2(
@@ -11,6 +13,8 @@ func _on_DebugOptions_resolution_selected(res) -> void:
 	var target_aspect_ratio = res[0] / res[1]
 
 	if change_aspect_ratio_only:
+		# get_viewport().size = Vector2(default_size.y * target_aspect_ratio, default_size.y)
 		OS.window_size = Vector2(default_size.y * target_aspect_ratio, default_size.y)
 	else:
+		# get_viewport().size = Vector2(res[0], res[1])
 		OS.window_size = Vector2(res[0], res[1])
