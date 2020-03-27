4c18d28547af9f5db3791d2bd81a667cc8d8656f
Davide Cristini <davcri91@gmail.com>
2020-03-27 10:32:17 +0100

    Show scores when the player destroys an alien

--- Objects/Score/Score.gd
+++ Objects/Score/Score.gd
@@ -6,6 +6,9 @@ export(int) var score_value setget set_score
 export(int) var distance = 0 setget set_distance
 
 
+signal score_hidden
+
+
 func set_score(value: int):
 	if !value is int:
 		print("Error in set_score: value has to be integer")
@@ -19,28 +22,14 @@ func set_distance(val):
 	_update_sprites()
 
 
-func _update_sprites(animate=true):
-	# remove old score sprites
-	for c in get_children():
-		c.queue_free()
-		remove_child(c)
-	# add new score sprites
-	for digit in str(score_value):
-		var num_spr: Sprite = _create_number(int(digit))
-		if get_child_count() > 0:
-			var last_num: Sprite = get_child(get_child_count() - 1)
-			num_spr.position.x = last_num.position.x + last_num.texture.get_size().x + distance
-		num_spr.name = "Sprite" + digit
-		add_child(num_spr)
-	if animate:
-		appear_animation()
-
-
 func appear_animation():
 	var delay = 0
+	# create tween
+	var tween = Tween.new()
+	add_child(tween)
+	# for each number sprite
 	for spr in get_children():
-		var tween = Tween.new()
-		add_child(tween)
+#		spr.modulate = Color(1,1,1,1)
 		tween.interpolate_property(
 			spr,
 			"scale",
@@ -52,8 +41,50 @@ func appear_animation():
 			delay
 		)
 		delay += 0.05
-		tween.start()
+	tween.start()
 
+
+func show():
+	.show()
+	appear_animation()
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(self,
+		"modulate:a", 1, 0, 0.5, Tween.TRANS_EXPO, Tween.EASE_IN, 2)
+	tween.start()
+	yield(tween, "tween_completed")
+	emit_signal("score_hidden", self)
+
+
+func _update_sprites(animate=false):
+	# remove old score sprites
+	for c in get_children():
+		remove_child(c)
+		c.queue_free()
+	# add new score sprites
+	var first_spr = null
+	var last_spr = null
+	for digit in str(score_value):
+		var num_spr: Sprite = _create_number(int(digit))
+		if first_spr == null:
+			first_spr = num_spr
+		# if at least one number was created
+		if get_child_count() > 0:
+			var previous: Sprite = get_child(get_child_count() - 1)
+			num_spr.position.x = previous.position.x + previous.texture.get_size().x + distance
+		num_spr.name = "Sprite" + digit
+		add_child(num_spr)
+		last_spr = num_spr
+	_center_sprites(first_spr, last_spr)
+	if animate:
+		appear_animation()
+
+
+func _center_sprites(first_spr: Sprite, last_spr: Sprite):
+	var width = last_spr.position.x + last_spr.texture.get_size().x - first_spr.position.x
+	# translate all the sprites to center them
+	for spr in get_children():
+		spr.position.x -= width / 2
 
 
 func _create_number(num) -> Sprite:
@@ -62,8 +93,4 @@ func _create_number(num) -> Sprite:
 	var texture_path = "res://Assets/graphics/kenney2/numbers/hud{n}.png".format({"n": num})
 	var texture = load(texture_path)
 	spr.texture = texture
-	spr.position = Vector2(100, 100)
 	return spr
-
-
-
--- Objects/Scores/Scores.gd
+++ Objects/Scores/Scores.gd
@@ -3,10 +3,28 @@ extends Node
 """ Handles the scores appearing when the projectile
 hits obstacles or enemies. """
 
+export(PackedScene) var score_scene
+
+var angle_randomness = 30
 
 func _ready():
 	pass
 
 
 func _on_Enemies_enemy_destroyed(enemy):
-	print(enemy)
+	var score_node: Node2D = _create_score()
+	add_child(score_node)
+	score_node.rotation_degrees = (randi() % angle_randomness) - (angle_randomness % 2)
+	score_node.global_position = enemy.global_position
+	score_node.score_value = 500 + randi() % 1000
+	score_node.show()
+	score_node.connect("score_hidden", self, "_on_score_hidden_remove_score")
+
+
+
+func _create_score():
+	return score_scene.instance()
+
+
+func _on_score_hidden_remove_score(score):
+	score.queue_free()
