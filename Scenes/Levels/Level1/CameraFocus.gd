55291f401c060f5f149564bdf2ad8191d3a3a2d9
Davide Cristini <davcri91@gmail.com>
2020-04-22 13:48:48 +0200

    Add camera pan

--- Scenes/Levels/Level1/CameraFocus.gd
+++ Scenes/Levels/Level1/CameraFocus.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-onready var slingshot = 0
+onready var slingshot = get_parent().get_node("Slingshot")
 
 export(float, 0.1, 3) var zoom = 1 setget set_zoom
 export var debug_move_with_wasd = true
@@ -11,20 +11,20 @@ var clamping = true
 
 func _ready():
 	set_zoom(zoom)
-	set_process(debug_move_with_wasd)
 
 
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		position.x -= speed * delta
-	if Input.is_action_pressed("ui_right"):
-		position.x += speed * delta
-	if Input.is_action_pressed("ui_up"):
-		position.y -= speed * delta
-	if Input.is_action_pressed("ui_down"):
-		position.y += speed * delta
-#	if Input.is_action_just_pressed("ui_select"):
-#		clamping = !clamping
+	if debug_move_with_wasd:
+		if Input.is_action_pressed("ui_left"):
+			position.x -= speed * delta
+		if Input.is_action_pressed("ui_right"):
+			position.x += speed * delta
+		if Input.is_action_pressed("ui_up"):
+			position.y -= speed * delta
+		if Input.is_action_pressed("ui_down"):
+			position.y += speed * delta
+	#	if Input.is_action_just_pressed("ui_select"):
+	#		clamping = !clamping
 	if clamping:
 		clamp_into_camera_limits()
 
@@ -38,7 +38,6 @@ func set_zoom(val):
 func clamp_into_camera_limits():
 #	print($Camera2D.get_canvas_transform().xform(Vector2($Camera2D.limit_left, $Camera2D.limit_top)))
 	var vws = $Camera2D.get_viewport().get_visible_rect().size * $Camera2D.zoom
-
 	position.x = clamp(position.x,
 		$Camera2D.limit_left + vws.x / 2,
 		$Camera2D.limit_right - vws.x / 2
@@ -47,3 +46,14 @@ func clamp_into_camera_limits():
 		$Camera2D.limit_top + vws.y / 2,
 		$Camera2D.limit_bottom - vws.y / 2
 	)
+
+
+func _unhandled_input(event):
+	# if user is aiming with the slinshot
+	if slingshot.state == slingshot.States.AIMING:
+		# don't move the camera
+		return
+
+	if event is InputEventScreenDrag:
+		position.x -= event.relative.x * $Camera2D.zoom.x
+		get_tree().set_input_as_handled()
