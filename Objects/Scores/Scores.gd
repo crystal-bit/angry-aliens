a61c1a4a0d9c5903e43b4ab55d212b22dc8a7f73
Davide Cristini <davcri91@gmail.com>
2020-04-24 20:25:53 +0200

    Fix a small error.
    Same kind of error needs to be fixed for VFX ParticleManager. But that could be rewritten taking advantage of the PoolableNode2D and Node2DPool scripts

--- Objects/Scores/Scores.gd
+++ Objects/Scores/Scores.gd
@@ -15,7 +15,8 @@ var angle_randomness = 30
 
 func _on_Enemies_enemy_destroyed(enemy):
 	var score_node: Node2D = scores_pool.get_instance()
-	add_child(score_node)
+	if score_node.get_parent() == null:
+		add_child(score_node)
 	score_node.rotation_degrees = (randi() % angle_randomness) - (angle_randomness % 2)
 	score_node.global_position = enemy.global_position
 	score_node.score_value = ENEMY_DESTROYED_POINTS + randi() % 100
