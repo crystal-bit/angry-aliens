a297da7735dd3064dc9434c351c6cf0d278294f4
Lucas HS <lucashilles@gmail.com>
2022-10-08 10:02:45 -0300

    chore(Level Selection): add audio player to Globals singleton

--- Globals/Globals.gd
+++ Globals/Globals.gd
@@ -2,8 +2,25 @@ extends Node
 
 
 var main_scene: Node
+var music_player: AudioStreamPlayer = AudioStreamPlayer.new()
 
 onready var current_level_index = 1
+
+
+func _ready():
+	var bgm_path = "res://Assets/music/monkeys-spinning-monkeys.ogg"
+	if File.new().file_exists(bgm_path):
+		var music = load(bgm_path)
+		music_player.stream = music
+		
+		var music_bus_id = AudioServer.get_bus_count()
+		AudioServer.add_bus()
+		AudioServer.set_bus_name(music_bus_id,"music")
+		# connects music to master bus
+		AudioServer.set_bus_send(music_bus_id,"Master")
+
+		add_child(music_player)
+		music_player.bus = "music"
 
 
 func goto_scene(new_scene: String, params = {}):
